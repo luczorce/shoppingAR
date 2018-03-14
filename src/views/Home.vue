e9e31125c0554230e8765af67de19f6080ba5be7
Ana Luczynski <ana.luczynski@salesforce.com>
2018-03-14 10:53:28 -0500

    mobile modifications and tests for camera capture
    * make video "square" as possible
    * change the manner in which the stream is processed with the video
    * add a play button for mobile devices to use

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,9 @@
     <p v-if="checkedCamera && noCamera">Sorry, you'll need a camera in order to experience this.</p>
 
     <video id="video" autoplay></video>
+    <p v-if="checkedCamera && !noCamera" class="centered">
+      <button type="button" v-on:click="playVideo">capture video</button>
+    </p>
   </div>
 </template>
 
@@ -17,7 +20,9 @@ export default {
       checkedCamera: false
     };
   },
-  methods: {},
+  methods: {
+    playVideo: playVideo
+  },
   mounted() {
     if (detectGetUserMedia()) {
       loadCamera();
@@ -31,27 +36,69 @@ export default {
 
 //////
 
+function caughtCameraStream(stream) {
+  // video.src = window.URL.createObjectURL(stream);
+  video.srcObject = stream;
+}
+
 // detect whether or not we have access to a camera
 function detectGetUserMedia() {
   return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 }
 
+function determineWindowSize() {
+  const offset = 50;
+  
+  // return { 
+  //   width: window.innerWidth - offset,
+  //   height: window.innerHeight - offset
+  // };
+
+  const square = window.innerWidth - offset;
+  return {
+    width: square,
+    height: square
+  }
+}
+
+function handleErrorInCamera(error) {
+  console.log(error);
+}
+
 // Get access to the camera!
 function loadCamera() {
+  let constraints, size;
+
   video = document.getElementById('video');
+  size = determineWindowSize();
+  constraints = {
+    video: {
+      width: {max: size.width},
+      height: {max: size.height},
+    }
+  }
+
+  video.width = size.width;
+  video.height = size.height;
   
   // Not adding `{ audio: true }` since we only want video now
-  navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-    video.src = window.URL.createObjectURL(stream);
-    video.play();
-  });
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(caughtCameraStream)
+    .catch(handleErrorInCamera);
 }
 
 // update our data to reflect the lack of camera
 function noCamera() {
   this.noCamera = true;
 }
+
+function playVideo() {
+  video.play();
+}
 </script>
 
 <style>
+  .centered {
+    text-align: center;
+  }
 </style>
